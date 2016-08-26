cd220b08dd86ecd38a5c4b9ed47227e8be2b3b57
ElucidSolutions <robert_sherwood@elucidsolutions.com>
2016-08-26 09:08:03 -0400

    Creating template for the community spotlight view.

---
+++ themes/achp/preserve_america/js/preserve_america.js
@@ -0,0 +1,20 @@
+/*
+ * @file
+ * This javascript randomly selects the community spotlight 
+ * to display in the preserve america section.
+ */
+ (function ($) {
+
+
+  /**
+   * The following code is executed when the DOM is fully
+   * loaded.
+   */
+   $(document).ready(function () {
+
+
+
+   });
+
+ })(jQuery);
+
