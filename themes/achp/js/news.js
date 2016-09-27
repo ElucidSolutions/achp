04a1c67ba3407172f7b057f0fcf3214278235546
restes19 <restes@elucidsolutions.com>
2016-09-27 17:14:04 -0400

    Have mobile + widescreen styling of news items mostly done; working on show/hide news filter function

--- themes/achp/js/news.js
+++ themes/achp/js/news.js
@@ -2,10 +2,23 @@
 
 (function ($) {
 
-  $("#edit-created-min").datepicker ();
-  $("#edit-created-max").datepicker ();
+  $("#edit-date-min").datepicker ();
+  $("#edit-date-max").datepicker ();
 
-  $("#edit-created-min").attr('value', 'Start date');
-  $("#edit-created-max").attr('value', 'End date');
+  $("#edit-date-min").attr('value', 'Start date');
+  $("#edit-date-max").attr('value', 'End date');
+
+  $('#edit-submit-latest-news').attr('value', 'Submit');
+
+  $('.news_filter').click ( function (e) {
+    if ($('.views-exposed-form').css('display') === 'none') {
+      $('.views-exposed-form').slideDown();
+      $(e.target).addClass('news_filter_open')
+    } else {
+      $('.views-exposed-form').slideUp();
+      $(e.target).delay(5000).removeClass('news_filter_open');
+      console.log('2');
+    };
+  });
 
 })(jQuery);
