0599a138a764edeb88d3c804988521e1d0bbcca2
restes19 <restes@elucidsolutions.com>
2016-10-06 16:11:57 -0400

    Working on slideshow module ...

---
+++ modules/custom/views_slideshow/js/slideshow.js
@@ -0,0 +1,19 @@
+(function ($) {
+
+  $(document).ready (function () {
+    // console.log($('.news-item-slideshow'));
+    // console.log(cycle);
+    // views_slideshow_slide 
+
+    // $('.views_slideshow_main').cycle();
+
+    $('.views_slideshow_main').fn.cycle.defaults = { 
+      timeout: 0
+    }
+
+
+    console.log('1');
+
+  })
+
+})(jQuery);
