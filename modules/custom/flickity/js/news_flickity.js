0caca3861e36cd5140205235b8f55eaec6a2dc86
restes19 <restes@elucidsolutions.com>
2016-10-12 17:02:24 -0400

    Wrote logical code (I think), but won't override flickity's translateX positioning

--- modules/custom/flickity/js/news_flickity.js
+++ modules/custom/flickity/js/news_flickity.js
@@ -14,10 +14,13 @@
     gallery item, and returns undefined.
   */
   function setCarouselDisplay () {
-    var numImages = getImageElements ().length;
+    // TODO: move this to a user-set variable in Drupal
     var maxImagesToDisplay = 4;
+
+    // var numImages = getImageElements ().length;
+    var numImages = getNumImages ();
     getNavGalleryItems ().forEach (function (galleryItem) {
-      $(galleryItem).attr ('data-news-num-images', Math.min(maxImagesToDisplay, numImages));
+      $(galleryItem).attr ('data-news-num-images-to-display', Math.min(maxImagesToDisplay, numImages));
     })
 
     if (numImages === 0) {
@@ -25,8 +28,117 @@
       hideNavCarousel ();
     } else if (numImages === 1) {
       hideNavCarousel (); 
-    }
+    } 
+    else { addSliderNavButtons ();}
   }
+
+  /*
+
+  */
+  function addSliderNavButtons () {
+    getNavSlider ()
+      .prepend ($('<div></div')
+        .addClass('slider-nav-button')
+        .addClass('prev')
+        .click (function () {
+          slideBackThroughNav ()
+        }))
+      .append ($('<div></div')
+        .addClass('slider-nav-button')
+        .addClass('next')
+        .click (function () {
+          slideForwardThroughNav ()
+        }))
+  }
+
+
+  /*
+    Accepts no arguments and returns a number representing
+    the percentage points of theslider's transform: translateX 
+    value.
+  */
+  function getSliderPosition () {
+    var navSlider = getNavSlider ();
+    var transformMatrix = 
+      navSlider.css("-webkit-transform") ||
+      navSlider.css("-moz-transform")    ||
+      navSlider.css("-ms-transform")     ||
+      navSlider.css("-o-transform")      ||
+      navSlider.css("transform");
+    transformMatrix.split(/[()]/)[1];
+    return transformMatrix.split(',')[4] / navSlider.width () * 100;
+  }
+
+  /*
+  */
+  function setSliderPosition (percentage) {
+    getNavSlider ().css('-webkit-transform', 'translateX:(' + percentage + '%)')
+      .css('-moz-transform', 'translateX: (' + percentage + '%)')
+      .css('-ms-transform', 'translateX:(' + percentage + '%)')
+      .css('-o-transform', 'translateX:(' + percentage + '%)')
+      .css('transform', 'translateX:(' + percentage + '%)');
+  }
+
+  /*
+  */
+  function slideBackThroughNav () {
+    var hiddenImages = getNumImages () - $(getNavGalleryItems ()[0]).attr('data-news-num-images-to-display');
+    var sliderPosition = getSliderPosition ();
+    console.log('At the start of slideBack, your position is: ' + getSliderPosition ());
+    
+    // If the current position, plus 1 image, is less than the width of the
+    // remaining images, move forward 1 image
+    if (Math.abs(sliderPosition + 25) < hiddenImages * 25) {
+      console.log('More than 1 image behind you. Should move to: ' + (sliderPosition - 25));
+      setSliderPosition (sliderPosition + 25);
+    // If the current position plus 1 image is greater to or equal than the
+    // remaining images, move to display the last image
+    } else if (Math.abs(sliderPosition + 25) >= hiddenImages * 25) {     
+      console.log('Less than 1 image behind you. Should move to: ' + -(hiddenImages * 25))
+      setSliderPosition (0);
+    } else if (sliderPosition === 0) {
+      console.log('Already at zero; this should be disabled')
+    }
+
+    console.log('At the end of slideBack, your position is: ' + getSliderPosition ());
+  }
+
+  /*
+  */
+  function slideForwardThroughNav () {
+    var hiddenImages = getNumImages () - $(getNavGalleryItems ()[0]).attr('data-news-num-images-to-display');
+    var sliderPosition = getSliderPosition ();
+    console.log('At the start of slideForward, your position is: ' + getSliderPosition ());
+    
+    // If the current position, plus 1 image, is less than the width of the
+    // remaining images, move forward 1 image
+    if (Math.abs(sliderPosition - 25) < hiddenImages * 25) {
+      console.log('More than 1 image to go. Should move to: ' + (sliderPosition - 25));
+      setSliderPosition (sliderPosition - 25);
+    // If the current position plus 1 image is greater to or equal than the
+    // remaining images, move to display the last image
+    } else if (Math.abs(sliderPosition - 25) > hiddenImages * 25) {     
+      console.log('Less than 1 image to go. Should move to: ' + -(hiddenImages * 25))
+      setSliderPosition (-(hiddenImages * 25));
+    } else if (Math.abs(sliderPosition) === hiddenImages * 25) {
+      console.log('You are already at the end ... I think');
+    }
+    console.log('At the end of slideForward, your position is: ' + getSliderPosition ());
+  }
+
+  /*
+  */
+  function getNumImages () {
+    return getImageElements ().length;
+  }
+
+  /*
+    Accepts no arguments and returns a jQuery HTML Element
+    that represents the navigator carousel's slider element.
+  */
+  function getNavSlider () {
+    return $('.' + getNavSliderClassName (), getNavCarousel ());
+  }  
 
   /*
     Accepts no arguments and returns an Array representing
@@ -74,6 +186,14 @@
   */
   function getImageElements () {
     return $('.field_news_photo');
+  }
+
+  /*
+    Accepts no arguments and returns a string representing
+    Flickity's slider element.
+  */
+  function getNavSliderClassName () {
+    return 'flickity-slider';
   }
 
   /*
