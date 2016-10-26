51ae8ff97435790a07a4fdc97297ba449f6970e7
Rebecca Estes <restes@elucidsolutions.com>
2016-10-26 15:20:31 -0400

    fixed line endings

--- themes/achp/js/events_landing.js
+++ themes/achp/js/events_landing.js
@@ -14,11 +14,15 @@
   /*
   */
   function reformatEventDates () {
-    var startDate = moment($(".field_event_start_date time").text ());
-    var endDate = moment($(".field_event_end_date time").text ());
-    var targetDiv = $(".field_event_end_date time");
-    var classPrefix = 'reformatted_event';
-    console.log(startDate)
+    var startDateString = $(".field_event_start_date time").text ();
+    var startDate = startDateString.slice (startDateString.indexOf(', ') + 2, startDateString.indexOf(' -'))
+      + ' ' + startDateString.slice (startDateString.indexOf(' - ') + 3);
+    // console.log(startDate);
+    // var startDate = moment($(".field_event_start_date time").text ());
+    // var endDate = moment($(".field_event_end_date time").text ());
+    // var targetDiv = $(".field_event_end_date time");
+    // var classPrefix = 'reformatted_event';
+    console.log(moment(startDate));
     // console.log(startDate.format ('MMMM D, YYYY'));
 
     // targetDiv.append($('<div></div>')
@@ -37,6 +41,11 @@
     //     .text (startDate.isSame (endDate, 'day') ?
     //       startDate.format ('h:mm A') + ' to ' + endDate.format ('h:mm A') :
     //       "")))
+  }
+
+  function formatDatesForMoment (dateString) {
+    return moment(dateString.slice (dateString.indexOf(', ') + 2, dateString.indexOf(' -'))
+      + ' ' + dateString.slice (dateString.indexOf(' - ') + 3));
   }
 
 }) (jQuery);
