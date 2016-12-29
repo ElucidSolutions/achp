35f86ba443c37bce5f02c2e0a6547b611a8346a2
Larry D. Lee jr <larry_lee@elucidsolutions.com>
2016-12-29 11:55:07 -0500

    Finished drafting stylesheets for the Digital Library view blocks and created the preprocess function that adds placeholder text and submit handler for the Digital Library views.

--- themes/achp/js/select.js
+++ themes/achp/js/select.js
@@ -11,16 +11,37 @@
   $(document).ready (function () { init (); });
 
   /*
-    Accepts no arguments, applies Chosen
-    to replace select elements, and returns
-    undefined.
+    Accepts no arguments, initializes the select
+    elements, and returns undefined.
   */
-  function init () { $('.' + getClassName ()).chosen (); }
+  function init () { getSelectElements ().chosen ({width: '100%'}); }
+
+  /*
+    Accepts one argument: selectElement,
+    a jQuery HTML Element that represents a
+    select element; initializes selectElement;
+    and returns undefined.
+  */
+  function initSelectElement (selectElement) { selectElement.chosen (); }
+
+  /*
+    Accepts no arguments and returns a jQuery
+    HTML Set representing the select elements
+    that should be rendered using Chosen.
+  */
+  function getSelectElements () { return $('.' + getSelectClassName ()); } 
 
   /*
     Accepts no arguments and returns a string
     that represents the class used to label
     select elements.
   */
-  function getClassName () { return 'achp-select'; }
+  function getSelectClassName () { return getClassPrefix (); }
+
+  /*
+    Accepts no arguments and returns a string
+    that represents the prefix used by all
+    classes defined by this module.
+  */
+  function getClassPrefix () { return 'achp-select'; }
 })(jQuery);
